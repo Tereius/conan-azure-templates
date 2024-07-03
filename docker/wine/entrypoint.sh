2682443db0032b72744d2e9583af2b7a07008404
Björn Stresing <bjoern.stresing@gmx.de>
2024-07-04 00:55:21 +0200

    Improve conanwine docker image

--- docker/wine/entrypoint.sh
+++ docker/wine/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 TMPFILE=$(mktemp /tmp/cmd-XXXXXXXX.bat)
-echo "$@" > $TMPFILE && . /opt/mkuserwineprefix && wine64 cmd /S /C $TMPFILE
+echo "@echo off\r\nset Python_ROOT_DIR=C:\\Python\r\n$@" > $TMPFILE && . /opt/mkuserwineprefix && wine64 cmd /S /C $TMPFILE
