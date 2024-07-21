edf21d012a6689ede99cf9b91ba442248ac48a78
Björn Stresing <bjoern.stresing@gmx.de>
2024-07-21 23:15:04 +0200

    Run command from tmpfile in entrypoint.sh

--- docker/ubuntu/entrypoint.sh
+++ docker/ubuntu/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-conan profile detect -vquiet
-exec bash -c "$@"
+TMPFILE=$(mktemp /tmp/cmd-XXXXXXXX.sh)
+echo "#!/bin/bash\nconan profile detect -vquiet\n$@" > $TMPFILE
+exec bash -e $TMPFILE
