03a15c415267bfcc48d5d0390518997ac8d16913
Björn Stresing <bjoern.stresing@gmx.de>
2024-07-29 22:04:44 +0200

    Update apt package cache in entrypoint.sh

--- docker/ubuntu/entrypoint.sh
+++ docker/ubuntu/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 TMPFILE=$(mktemp /tmp/cmd-XXXXXXXX.sh)
-echo "#!/bin/bash\nconan profile detect -vquiet\n$@" > $TMPFILE
+echo "#!/bin/bash\nsudo apt-get -y -q update\nconan profile detect -vquiet\n$@" > $TMPFILE
 exec bash -e $TMPFILE
